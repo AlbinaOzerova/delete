898081213fc1916fd07c3cf5f9497cf19b4ecc3c
Albina <workozerova@gmail.com>
2025-06-26 14:23:50 +0100

    truthy_falthy activity

--- hello.py
+++ hello.py
@@ -1,1 +1,45 @@
-print("hello")
+# print("Hello")
+
+# var_name = []
+# var_name = "Hello"
+# var_name = 0
+# var_name = ""
+# print(bool(var_name))
+
+# name = input("Enter your name - ")
+# if bool(name) == False:
+#     print("You didn't enter the name")
+# else:
+#   
+# print(f"Thanks for signing up {name}")
+
+# name = input("Enter your name - ")
+
+# if name:
+#     print(f"Thanks for signing up {name}")
+# else:
+#     print("You didn't enter the name")
+
+
+# print ("What is your name?")
+# name = input(" > ")
+# if name:
+#     print(f"Hello {name}, how are you?")
+# else:
+#     print( "You did not give me your name!")
+
+# print ("What is your name?")
+# name = input(" > ")
+# if bool(name) == True:
+#     print(f"Hello {name}, how are you?")
+# else:
+#     print( "You did not give me your name!")
+
+print ("What is your name?")
+name = input(" > ")
+if name and name != " ":
+    print(f"Hello {name.capitalize()}, how are you?")
+else:
+    print( "You did not give me your name!")
+
+
