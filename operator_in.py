e36a725288963308092f765ef4815eafb14a3773
Albina <workozerova@gmail.com>
2025-06-26 15:21:46 +0100

    add not in operator code

--- operator_in.py
+++ operator_in.py
@@ -6,3 +6,15 @@ if "milk" in shopping_list:
     print("Milk is on the shopping list")
 else:
     print("Milk is missing")
+
+movie_titles = ["rocky", "warstars", "batman"]
+
+print("Add a new movie title")
+new_movie = input(" > ")
+
+if new_movie not in movie_titles:
+    movie_titles.append(new_movie)
+else:
+    print(f"The movie {new_movie} is already on the list")
+
+print(movie_titles)
